ce2f133911495c3b9f6413ad5ba75fc1235797d1
Peter Molnar <petermolnar.hu@gmail.com>
2021-11-07 18:42:56 +0100

    Initial Commit

---
+++ MultiplAce/MultiplAceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplAceApp.swift
+//  MultiplAce
+//
+//  Created by Peter Molnar on 07/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MultiplAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
